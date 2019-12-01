abafde2ffdb10e1b7069d2973e0e36b9a264a3ce
Marick van Tuil <info@marickvantuil.nl>
2019-12-01 19:04:36 +0100

    Initial commit

---
+++ docker/app/newrelic.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+##--------------------------------------------------------
+# CHECK IF NV_INSTALL_KEY IS NOT DEFAULT TO INSTALL NR APM
+##--------------------------------------------------------
+
+if [ "$NR_INSTALL_KEY" = "**ChangeMe**" ]; then
+    echo "Skipping New Relic Setup..."
+else
+    echo "New Relic NR_INSTALL_KEY detected..."
+    echo "Enabling APM metrics for ${NR_APP_NAME}"
+    newrelic-install install
+
+    # Update the application name
+    sed -i "s/newrelic.appname = \"PHP Application\"/newrelic.appname = \"${NR_APP_NAME}\"/" /etc/php/7.3/fpm/conf.d/newrelic.ini
+
+    # Remove extra new relic config file (causes log spamming)
+    rm /etc/php/7.3/cli/conf.d/newrelic.ini
+fi
---
+++ docker/app/phpfpm.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+php-fpm7.3 -c /etc/php/7.3/fpm
